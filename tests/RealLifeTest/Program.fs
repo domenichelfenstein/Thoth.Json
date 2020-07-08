5ad8643e706d1ae0495e6aed90180872414ac797
Domenic Helfenstein <d.helfenstein@calitime.ch>
2020-07-08 14:27:34 +0200

    uppercase guid codecs

--- tests/RealLifeTest/Program.fs
+++ tests/RealLifeTest/Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Newtonsoft.Json.Linq
 open RealLifeTest.Types
 open Thoth.Json.Newtonsoft
 
@@ -27,11 +28,34 @@ let main argv =
             }
             FullName "Maggie Simpson"
         ]
-        WorkRelationship = WorkRelationship.OpenEnd (OpenEnd.OpenEnd "1990-09-01")
+        WorkRelationship = WorkRelationship.Terminated ("1990-01-01", "2010-04-05")
     }
 
-    let json = Encode.Auto.toString(4, operationData, caseStrategy=CaseStrategy.CamelCase)
-    let deserialized = Decode.Auto.unsafeFromString(json, caseStrategy=CaseStrategy.CamelCase)
+    let tryParseGuid (guidString: string) =
+        let parseResult, guid = Guid.TryParse guidString
+        if parseResult then
+            Some guid
+        else
+            None
+
+    let isGuid (json: JsonValue) =
+        not(isNull json) && json.Type = JTokenType.String
+
+    let extra =
+        Extra.empty
+        |> Extra.withCustom
+               (fun guid -> JValue(guid.ToString().ToUpper()) :> JsonValue)
+               (fun json ->
+                    if isGuid json then
+                        let guidString = json.Value<string>()
+                        match guidString |> tryParseGuid with
+                        | Some guid -> Ok guid
+                        | None ->  Error <| (guidString, FailMessage("content is no guid"))
+                    else
+                        Error <| (json.ToString(), FailMessage("content is no string")))
+
+    let json = Encode.Auto.toString(4, operationData, caseStrategy=CaseStrategy.CamelCase, extra=extra)
+    let deserialized = Decode.Auto.unsafeFromString(json, caseStrategy=CaseStrategy.CamelCase, extra=extra)
 
     let result = operationData = deserialized
 
