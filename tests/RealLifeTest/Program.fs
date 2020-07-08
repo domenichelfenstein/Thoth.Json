282e799706cbd81dbc1ae41c8313eedba6bba9ff
Domenic Helfenstein <d.helfenstein@calitime.ch>
2020-07-08 13:45:16 +0200

    real life complex type encoding and decoding

---
+++ tests/RealLifeTest/Program.fs
@@ -0,0 +1,40 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open RealLifeTest.Types
+open Thoth.Json.Newtonsoft
+
+[<EntryPoint>]
+let main argv =
+    let operationData = {
+        CreateOperationData.Employee = EmployeeId (Guid.NewGuid())
+        Gender = Male
+        Name = ComplexName {
+            FirstName = "Homer"
+            MiddleInitial = Some "J"
+            LastName = "Simpson"
+        }
+        DistanceToOffice = Distance 9.56
+        Children = [
+            SimpleName {
+                FirstName = "Bart"
+                LastName = "Simpson"
+            }
+            ComplexName {
+                FirstName = "Lisa"
+                MiddleInitial = None
+                LastName = "Simpson"
+            }
+            FullName "Maggie Simpson"
+        ]
+        WorkRelationship = WorkRelationship.OpenEnd (OpenEnd.OpenEnd "1990-09-01")
+    }
+
+    let json = Encode.Auto.toString(4, operationData, caseStrategy=CaseStrategy.CamelCase)
+    let deserialized = Decode.Auto.unsafeFromString(json, caseStrategy=CaseStrategy.CamelCase)
+
+    let result = operationData = deserialized
+
+    printfn "%s %b" json result
+    0 // return an integer exit code
+
---
+++ tests/RealLifeTest/Types.fs
@@ -0,0 +1,44 @@
+﻿module RealLifeTest.Types
+
+    open System
+
+    type EmployeeId = EmployeeId of Guid
+    type Gender =
+        | Female
+        | Male
+        | Other
+
+    type SimpleName = {
+        FirstName: string
+        LastName: string
+    }
+    type ComplexName = {
+        FirstName: string
+        MiddleInitial: string option
+        LastName: string
+    }
+    type Name =
+        | FullName of string
+        | SimpleName of SimpleName
+        | ComplexName of ComplexName
+
+    [<Measure>]
+    type km
+
+    type Distance = Distance of double
+
+    type OpenEnd = OpenEnd of string
+
+    type WorkRelationship =
+        | OpenEnd of OpenEnd
+        | Terminated of string * string
+
+    type CreateOperationData = {
+        Employee: EmployeeId
+        Gender: Gender
+        Name: Name
+        DistanceToOffice: Distance
+        Children: Name list
+        WorkRelationship: WorkRelationship
+    }
+
